162c5ca9c532c85934739daf439ddcbb929e971d
Jon Smith <aardvarkjon94@yahoo.com>
2021-05-04 20:40:01 -0500

    Added Fillable form section

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ doc.pipe(fs.createWriteStream('output.pdf'));
 
 doc
     .fontSize(25)
-    .text('Some text with an embedded font!', 100, 100);
+    .text('Dungeons and Dragons', 10, 50);
+
+
+doc
+    .initForm()
+    .fontSize(12)
+    .text("Character Name:", 100, 120)
+    .formText("name", 100, 90, 200, 30)
 
 doc.end();
