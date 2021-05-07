7c7f31fbb48d12669813e00274ad864a5f643317
Jon Smith <aardvarkjon94@yahoo.com>
2021-05-06 20:23:44 -0500

    finished character info section

--- index.js
+++ index.js
@@ -14,7 +14,24 @@ doc
 doc
     .initForm()
     .fontSize(12)
-    .text("Character Name:", 100, 30)
-    .formText("name", 100, 40, 200, 30)
-
+    .text("Character Name:", 30, 40)
+    .formText("name",30, 50, 200, 30)
+    //Character class and level
+    .text("Class & Level", 300, 40)
+    .formText("class_level", 300, 20, 100, 20)
+    //Character Background
+    .text("Background", 400, 40)
+    .formText("Background", 400, 20, 100, 20)
+    //Player Name
+    .text("Player Name", 500, 40,{width: 100})
+    .formText("player_name", 500, 20, 100, 20)
+    //Character race
+    .text("Race", 300, 90)
+    .formText("race", 300, 60, 100, 20)
+    //Character Alignment
+    .text("Alignment", 400, 90)
+    .formText("alignment", 400, 60, 100, 20)
+    //Character experience
+    .text("Experience Points", 500, 90, {width: 100})
+    .formText("Experience", 500, 60, 100, 20)
 doc.end();
