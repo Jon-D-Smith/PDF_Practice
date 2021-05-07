f49b377dcee91da5be8a4d51867f39db9f89ba09
Jon Smith <aardvarkjon94@yahoo.com>
2021-05-06 20:41:30 -0500

    Added stats section

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const doc = new PDFDocument();
 
 doc.pipe(fs.createWriteStream('output.pdf'));
 
+//Title Text
 doc
     .fontSize(25)
     .text('Dungeons and Dragons', 10, 10);
@@ -34,4 +35,33 @@ doc
     //Character experience
     .text("Experience Points", 500, 90, {width: 100})
     .formText("Experience", 500, 60, 100, 20)
+
+
+//Character Stats
+doc
+    
+    .fontSize(8)
+    //Strength
+    .text("Strength", 45, 100)
+    .formText('Strength', 40, 110,40,40)
+    //Dexterity
+    .text("Dexterity", 45, 160)
+    .formText('Dexterity', 40, 170,40,40)
+    //Constitution
+    .text("Constitution", 45, 220)
+    .formText('Constitution', 40, 230,40,40)
+    //Intelligence
+    .text("Intelligence", 45, 280)
+    .formText('Intelligence', 40, 290,40,40)
+    //Wisdom
+    .text("Wisdom", 45, 340)
+    .formText('Wisdom', 40, 350,40,40)
+    //Charisma
+    .text("Charisma", 45, 400)
+    .formText('Charisma', 40, 410,40,40)
+
+
+
+
+
 doc.end();
